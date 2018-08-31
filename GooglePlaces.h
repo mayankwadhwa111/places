2e0f1941089787c71c72510cd220399f5de807f5
mayankwadhwa111 <42866329+mayankwadhwa111@users.noreply.github.com>
2018-08-31 17:00:30 +0530

    Add files via upload

---
+++ GooglePlaces.h
@@ -0,0 +1,35 @@
+//
+//  GooglePlaces.h
+//  GooglePlaces
+//
+//  Created by Leone Parise Vieira da Silva on 29/09/16.
+//  Copyright © 2016 com.google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GooglePlaces.
+FOUNDATION_EXPORT double GooglePlacesVersionNumber;
+
+//! Project version string for GooglePlaces.
+FOUNDATION_EXPORT const unsigned char GooglePlacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GooglePlaces/PublicHeader.h>
+
+#import <GooglePlaces/GMSAddressComponent.h>
+#import <GooglePlaces/GMSAutocompleteFetcher.h>
+#import <GooglePlaces/GMSAutocompleteFilter.h>
+#import <GooglePlaces/GMSAutocompleteMatchFragment.h>
+#import <GooglePlaces/GMSAutocompletePrediction.h>
+#import <GooglePlaces/GMSAutocompleteResultsViewController.h>
+#import <GooglePlaces/GMSAutocompleteTableDataSource.h>
+#import <GooglePlaces/GMSAutocompleteViewController.h>
+#import <GooglePlaces/GMSPlace.h>
+#import <GooglePlaces/GMSPlaceLikelihood.h>
+#import <GooglePlaces/GMSPlaceLikelihoodList.h>
+#import <GooglePlaces/GMSPlacePhotoMetadata.h>
+#import <GooglePlaces/GMSPlacePhotoMetadataList.h>
+#import <GooglePlaces/GMSPlaceTypes.h>
+#import <GooglePlaces/GMSPlacesClient.h>
+#import <GooglePlaces/GMSPlacesErrors.h>
+#import <GooglePlaces/GMSUserAddedPlace.h>
